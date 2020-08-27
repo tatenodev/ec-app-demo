7cfcc033c1f525d1a50acf5230d7de19b3a9326a
wd30gsrc <gd28.c.tateno@gmail.com>
2020-08-27 21:32:30 +0900

    re-ducksパターン

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {getUserId} from '../reducks/users/selector'
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+    const selector = useSelector(state => state);
+    const uid = getUserId(selector)
+
     return (
+    <div>
         <h2>Home</h2>
+        <p>{uid}</p>
+    </div>
     )
 }
 export default Home
